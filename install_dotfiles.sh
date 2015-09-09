c3fa0a189265c070c5c506e4dc7cebe22027698a
Daniel <danielgoodnow@gmail.com>
2015-09-09 15:43:51 -0700

    add ycm conf file

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -10,7 +10,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc gitconfig"    # list of files/folders to symlink in homedir
+files="bashrc vimrc gitconfig ycm_extra_conf.py"    # list of files/folders to symlink in homedir
 
 ##########
 
